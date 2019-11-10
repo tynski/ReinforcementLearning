8281d76d5374f7ccc0b428aad9e127e47ecc0669
tynski <b.tynski@gmail.com>
2019-11-10 17:52:15 +0100

    multiple charts

--- qtableviz.py
+++ qtableviz.py
@@ -14,22 +14,22 @@ def get_q_color(value, vals):
 
 fig = plt.figure(figsize=(12, 9))
 
-ax1 = fig.add_subplot(311)
-ax2 = fig.add_subplot(312)
-ax3 = fig.add_subplot(313)
+for i in range(0, 4000, 100):
+    ax1 = fig.add_subplot(311)
+    ax2 = fig.add_subplot(312)
+    ax3 = fig.add_subplot(313)
 
-i = 100 
-q_table = np.load("qtables/{}-qtable.npy".format(i))
+    q_table = np.load("qtables/{}-qtable.npy".format(i))
 
-for x, x_vals in enumerate(q_table):
-    for y, y_vals in enumerate(x_vals):
-        ax1.scatter(x, y, c=get_q_color(y_vals[0], y_vals)[0], marker="o", alpha=get_q_color(y_vals[0], y_vals)[1])
-        ax2.scatter(x, y, c=get_q_color(y_vals[1], y_vals)[0], marker="o", alpha=get_q_color(y_vals[1], y_vals)[1])
-        ax3.scatter(x, y, c=get_q_color(y_vals[2], y_vals)[0], marker="o", alpha=get_q_color(y_vals[2], y_vals)[1])
+    for x, x_vals in enumerate(q_table):
+        for y, y_vals in enumerate(x_vals):
+            ax1.scatter(x, y, c=get_q_color(y_vals[0], y_vals)[0], marker="o", alpha=get_q_color(y_vals[0], y_vals)[1])
+            ax2.scatter(x, y, c=get_q_color(y_vals[1], y_vals)[0], marker="o", alpha=get_q_color(y_vals[1], y_vals)[1])
+            ax3.scatter(x, y, c=get_q_color(y_vals[2], y_vals)[0], marker="o", alpha=get_q_color(y_vals[2], y_vals)[1])
 
-        ax1.set_ylabel("Action 0")
-        ax2.set_ylabel("Action 1")
-        ax3.set_ylabel("Action 2")
+            ax1.set_ylabel("Action 0")
+            ax2.set_ylabel("Action 1")
+            ax3.set_ylabel("Action 2")
 
-
-plt.show()
+    plt.savefig("qtable_charts/{}.png".format(i))
+    plt.clf()
